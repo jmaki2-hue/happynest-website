06ae5ee3ff676200cdafb457bda4f364db34d93b
John Makinano <jmakinano.arthur@gmail.com>
2026-05-10 17:25:28 +0800

    updated website

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       image: "/Bead me up.jpg",
       desc:
         "Customized handmade bead souvenirs perfect for birthdays, weddings, baby showers, and special occasions.",
-      link: "#",
+      link: "https://bead-me-up.vercel.app/",
     },
 
     {
